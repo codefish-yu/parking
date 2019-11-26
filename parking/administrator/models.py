dbac9781e19d177c81446936739cfc87f4c09558
杜膳辰 <dsc@macdeMacBook-Pro-5.local>
2019-11-26 16:36:45 +0800

    gogo

--- parking/administrator/models.py
+++ parking/administrator/models.py
@@ -93,5 +93,37 @@ class Options(models.Model):
 class Log(models.Model):
     class Meta:
         verbose_name = verbose_name_plural = '操作日志'
+        indexes = [
+            models.Index(
+                fields=['create_day'],
+                name='create_day_idx',
+            ),
+        ]
+    ip = models.CharField(max_length=100, null=True, blank=True, verbose_name='ip')
+    operation = models.CharField(max_length=100, null=True, blank=True, verbose_name='操作')
+    content = models.CharField(max_length=100, null=True, blank=True, verbose_name='操作内容')
+    model = models.CharField(max_length=100, null=True, blank=True, verbose_name='操作模块')
+    user = models.ForeignKey(on_delete=models.SET_NULL, null=True, blank=True, verbose_name='操作用户')
 
-    pass
+    create_time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    create_day = models.IntegerField(null=True, blank=True)
+
+
+class Worker(models.Model):
+    class Meta:
+        verbose_name = verbose_name_plural = '车场员工'
+
+    number = models.CharField(max_length=100, unique=True, null=True, blank=True, verbose_name='员工号')
+    name = models.CharField(max_length=100, null=True, blank=True, verbose_name='员工名称')
+    birth = models.DateField(null=True, blank=True, verbose_name='出生日期')
+    sex = models.IntegerField(choices=[(0, '男'),(1, '女')], verbose_name='性别')
+
+    is_delete = models.IntegerField(choices=[(0, '否'),(1, '是')], verbose_name='是否删除')
+    forbidden = models.IntegerField(choices=[(0, '否'),(1, '是')], verbose_name='是否禁用')
+    # parkinglot = models.ForeignKey('Parkinglot',  on_delete=models.SET_NULL, verbose_name='所属车场')
+
+    create_time = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    update_time = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+
+
--- parking/administrator/views.py
+++ parking/administrator/views.py
@@ -146,6 +146,53 @@ def save_auth(r, request):
     return
 
 
+@page
+def worker(request):
+    ctx = {}
+
+    workers = Worker.objects.all()
+
+    if request.method == 'POST':
+        action = request.POST.get('action', '')
+        if action == 'add':
+            r = Worker()
+            _save_attr_(r, request)
+
+
+        elif action == 'update':
+            id = request.POST.get('id', '')
+
+            r = Worker.objects.filter(id=id).first()
+            _save_attr_(r, request)
+            save_auth(r, request)
+
+        elif action == 'search':
+            ctx['worker_name'] = worker_name = request.POST.get('worker_name', '')
+            roles = Worker.objects.filter(worker_name__contains=worker_name.strip())
+
+        elif action == 'delete':
+            ids = request.POST.getlist('ids', '')
+            Worker.objects.filter(id__in=ids).delete()
+
+        elif action == 'validate':
+            number = request.POST.get('number', '')
+            id = request.POST.get('id', '')
+
+            r = Worker.objects.filter(number=number.strip())
+            if r.exists():
+                if id:
+                    if r.first().id != int(id):
+                        return JsonResponse({'valid': False})
+                else:
+                    return JsonResponse({'valid': False})
+
+            return JsonResponse({'valid': True})
+
+    ctx['objects'] = workers
+
+    return (ctx, 'worker.html')
+
+
 
 def _save_attr_(obj,request):
     fields = obj._meta.fields
