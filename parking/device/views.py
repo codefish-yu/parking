f023cc64229f0fdd48776c653cb82543935d3cb7
杜膳辰 <dsc@macdeMacBook-Pro-5.local>
2019-11-28 10:24:49 +0800

    设备管理

--- parking/device/views.py
+++ parking/device/views.py
@@ -1,6 +1,225 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
 
+from .models import *
+from administrator.decorators import page, _save_attr_
 
+ 
 '''设备管理模块'''
+
+
+@csrf_exempt
+@page
+def camera(request):
+    '''摄像头管理'''
+
+    ctx = {}
+
+    cameras = Camera.objects.select_related('parkinglot','gate').all()
+
+    if request.method == 'POST':
+        action = request.POST.get('action', '')
+        if action == 'add':
+            r = Camera()            
+            _save_attr_(r, request)
+
+            parkinglot_id = request.POST.get('parkinglot_id', '')
+            if parkinglot_id:
+                p = ParkingLot.objects.filter(id=parkinglot_id).first()
+                if p: 
+                    r.parkinglot = p
+                    r.save()
+
+            gate_id = request.POST.get('gate_id', '')
+            if gate_id:
+                p = Gate.objects.filter(id=gate_id).first()
+                if p: 
+                    r.gate = p
+                    r.save()
+
+        elif action == 'update':
+            id = request.POST.get('id', '')
+
+            r = Camera.objects.filter(id=id).first()
+            _save_attr_(r, request)
+            parkinglot_id = request.POST.get('parkinglot_id', '')
+            if parkinglot_id:
+                p = ParkingLot.objects.filter(id=parkinglot_id).first()
+                if p: 
+                    r.parkinglot = p
+                    r.save()
+            gate_id = request.POST.get('gate_id', '')
+            if gate_id:
+                p = Gate.objects.filter(id=gate_id).first()
+                if p: 
+                    r.gate = p
+                    r.save()
+        elif action == 'search':
+            ctx['brand'] = brand = request.POST.get('brand', '')
+            ctx['manufacturer'] = manufacturer = request.POST.get('manufacturer', '')
+           
+            gate_id = request.POST.get('gate_id', '')
+            parkinglot_id = request.POST.get('parkinglot_id', '')
+
+            if parkinglot_id:
+                ctx['parkinglot_id'] = int(parkinglot_id)
+                cameras = cameras.filter(parkinglot_id=int(parkinglot_id))
+                ctx['gates'] = Gate.objects.filter(parkinglot_id=int(parkinglot_id))
+            if gate_id:
+                ctx['gate_id'] = int(gate_id)
+                cameras = cameras.filter(gate_id=int(gate_id))
+            if brand:
+                cameras = cameras.filter(brand=brand)
+            if manufacturer:
+                cameras = cameras.filter(manufacturer=manufacturer)
+
+        elif action == 'delete':
+            ids = request.POST.getlist('ids', '')
+            Camera.objects.filter(id__in=ids).delete()
+
+        elif action == 'validate':
+            number = request.POST.get('number', '')
+            id = request.POST.get('id', '')
+
+            r = Camera.objects.filter(number=number.strip())
+            if r.exists():
+                if id:
+                    if r.first().id != int(id):
+                        return JsonResponse({'valid': False})
+                else:
+                    return JsonResponse({'valid': False})
+
+            return JsonResponse({'valid': True})
+
+        elif action == 'get_gate':
+            id = request.POST.get('id', '')
+            gates = Gate.objects.filter(parkinglot_id=id)
+            gates = [{'name': _.name, 'id':_.id} for _ in gates]
+            return JsonResponse({'success':True, 'result': gates})
+
+    ctx['objects'] = cameras.order_by('-buy_time')
+    ctx['parkinglots'] = ParkingLot.objects.filter(status=0).all()
+    print(ctx['parkinglots'])
+    all_gates = {}
+
+    gates = Gate.objects.select_related('parkinglot').filter(parkinglot__status=0).order_by('parkinglot')
+    for i in gates:
+        if i.parkinglot.id in all_gates:
+            all_gates[i.parkinglot.id].append({'gate_id': i.id, 'gate_name': i.monitor})
+        else:
+            all_gates[i.parkinglot.id] = [{'gate_id': i.id, 'gate_name': i.monitor}]
+    ctx['all_gates'] = all_gates
+
+    return (ctx, 'camera.html')
+
+
+@csrf_exempt
+@page
+def brake(request):
+    '''闸机管理'''
+
+    ctx = {}
+
+    brakes = Brake.objects.select_related('parkinglot','gate').all()
+
+    if request.method == 'POST':
+        action = request.POST.get('action', '')
+        if action == 'add':
+            r = Brake()            
+            _save_attr_(r, request)
+
+            parkinglot_id = request.POST.get('parkinglot_id', '')
+            if parkinglot_id:
+                p = ParkingLot.objects.filter(id=parkinglot_id).first()
+                if p: 
+                    r.parkinglot = p
+                    r.save()
+
+            gate_id = request.POST.get('gate_id', '')
+            if gate_id:
+                p = Gate.objects.filter(id=gate_id).first()
+                if p: 
+                    r.gate = p
+                    r.save()
+
+        elif action == 'update':
+            id = request.POST.get('id', '')
+
+            r = Brake.objects.filter(id=id).first()
+            _save_attr_(r, request)
+            parkinglot_id = request.POST.get('parkinglot_id', '')
+            if parkinglot_id:
+                p = ParkingLot.objects.filter(id=parkinglot_id).first()
+                if p: 
+                    r.parkinglot = p
+                    r.save()
+            gate_id = request.POST.get('gate_id', '')
+            if gate_id:
+                p = Gate.objects.filter(id=gate_id).first()
+                if p: 
+                    r.gate = p
+                    r.save()
+        elif action == 'search':
+            ctx['brand'] = brand = request.POST.get('brand', '')
+            ctx['manufacturer'] = manufacturer = request.POST.get('manufacturer', '')
+           
+            gate_id = request.POST.get('gate_id', '')
+            parkinglot_id = request.POST.get('parkinglot_id', '')
+
+            if parkinglot_id:
+                ctx['parkinglot_id'] = int(parkinglot_id)
+                brakes = brakes.filter(parkinglot_id=int(parkinglot_id))
+                ctx['gates'] = Gate.objects.filter(parkinglot_id=int(parkinglot_id))
+            if gate_id:
+                ctx['gate_id'] = int(gate_id)
+                brakes = brakes.filter(gate_id=int(gate_id))
+            if brand:
+                brakes = brakes.filter(brand=brand)
+            if manufacturer:
+                brakes = brakes.filter(manufacturer=manufacturer)
+                
+        elif action == 'delete':
+            ids = request.POST.getlist('ids', '')
+            Brake.objects.filter(id__in=ids).delete()
+
+        elif action == 'validate':
+            number = request.POST.get('number', '')
+            id = request.POST.get('id', '')
+
+            r = Brake.objects.filter(number=number.strip())
+            if r.exists():
+                if id:
+                    if r.first().id != int(id):
+                        return JsonResponse({'valid': False})
+                else:
+                    return JsonResponse({'valid': False})
+
+            return JsonResponse({'valid': True})
+
+        elif action == 'get_gate':
+            id = request.POST.get('id', '')
+            gates = Gate.objects.filter(parkinglot_id=id)
+            gates = [{'name': _.name, 'id':_.id} for _ in gates]
+            return JsonResponse({'success':True, 'result': gates})
+
+    ctx['objects'] = brakes.order_by('-buy_time')
+    ctx['parkinglots'] = ParkingLot.objects.filter(status=0).all()
+    print(ctx['parkinglots'])
+    all_gates = {}
+
+    gates = Gate.objects.select_related('parkinglot').filter(parkinglot__status=0).order_by('parkinglot')
+    for i in gates:
+        if i.parkinglot.id in all_gates:
+            all_gates[i.parkinglot.id].append({'gate_id': i.id, 'gate_name': i.monitor})
+        else:
+            all_gates[i.parkinglot.id] = [{'gate_id': i.id, 'gate_name': i.monitor}]
+    ctx['all_gates'] = all_gates
+
+    return (ctx, 'brake.html')
+
+
+
+def groundsensor(request):
+    pass
--- parking/parking/urls.py
+++ parking/parking/urls.py
@@ -21,13 +21,16 @@ from django.conf import settings
 
 from administrator import views as a
 from car import views as car
+from device import views as device
 from parkinglot import views as park
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
 
     # administrator
+
     path('',a.base),
     path('login/',a.login),
     path('base/', a.base),   
@@ -36,21 +39,32 @@ urlpatterns = [
     path('administrator/role/<int:id>/', a.get_role),
     path('administrator/user/',a.user),
     path('administrator/modify/',a.modify),
+    
+
     # user
 
 
     # car 
+
     path('car/inner_car/', car.inner_car),
     
 
 
     
-    #parkinglot
+    # parkinglot
+
     path('parkinglot/parking_lot/',park.parking_lot),
     path('parkinglot/gate/',park.gate),
     path('parkinglot/worker/', park.worker),
     path('parkinglot/zone/',park.zone),
     path('parkinglot/place/',park.place),
+
+
+    # device
+
+    path('device/brake/',device.brake),
+    path('device/camera/',device.camera),
+    path('device/groundsensor/',device.groundsensor),
 
 
 
