2928da0929e82c66f8dbd1bee8a9a4b4873a313b
杜膳辰 <dsc@macdeMacBook-Pro-5.local>
2019-11-28 14:12:04 +0800

    gogo

---
+++ parking/chargerule/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ChargeruleConfig(AppConfig):
+    name = 'chargerule'
---
+++ parking/chargerule/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from .models import *
+
+
+'''计费规则管理'''
+
+
+def base_rule(request):
+    '''基本计费规则配置'''
+    pass
+
+
+def card_type(request):
+    '''卡片类型设置'''
+    pass
+
+
+def coupon_type(request):
+    '''优惠券类型设置'''
+    pass
+
+
+def card(request):
+    '''卡片管理'''
+    pass
+
+
+def coupon(request):
+    '''优惠券管理'''
+    pass
+
