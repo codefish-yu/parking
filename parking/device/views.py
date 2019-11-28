afea4d242435c29969a31a40b25a214b3a143231
杜膳辰 <dsc@macdeMacBook-Pro-5.local>
2019-11-28 11:00:46 +0800

    gogo

--- parking/device/views.py
+++ parking/device/views.py
@@ -220,6 +220,106 @@ def brake(request):
     return (ctx, 'brake.html')
 
 
-
+@csrf_exempt
+@page
 def groundsensor(request):
-    pass
+    '''地感管理'''
+
+    ctx = {}
+
+    groundsensors = GroundSensor.objects.select_related('parkinglot','gate').all()
+
+    if request.method == 'POST':
+        action = request.POST.get('action', '')
+        if action == 'add':
+            r = GroundSensor()            
+            _save_attr_(r, request)
+
+            parkinglot_id = request.POST.get('parkinglot_id', '')
+            if parkinglot_id:
+                p = ParkingLot.objects.filter(id=parkinglot_id).first()
+                if p: 
+                    r.parkinglot = p
+                    r.save()
+
+            gate_id = request.POST.get('gate_id', '')
+            if gate_id:
+                p = Gate.objects.filter(id=gate_id).first()
+                if p: 
+                    r.gate = p
+                    r.save()
+
+        elif action == 'update':
+            id = request.POST.get('id', '')
+
+            r = GroundSensor.objects.filter(id=id).first()
+            _save_attr_(r, request)
+            parkinglot_id = request.POST.get('parkinglot_id', '')
+            if parkinglot_id:
+                p = ParkingLot.objects.filter(id=parkinglot_id).first()
+                if p: 
+                    r.parkinglot = p
+                    r.save()
+            gate_id = request.POST.get('gate_id', '')
+            if gate_id:
+                p = Gate.objects.filter(id=gate_id).first()
+                if p: 
+                    r.gate = p
+                    r.save()
+        elif action == 'search':
+            ctx['brand'] = brand = request.POST.get('brand', '')
+            ctx['manufacturer'] = manufacturer = request.POST.get('manufacturer', '')
+           
+            gate_id = request.POST.get('gate_id', '')
+            parkinglot_id = request.POST.get('parkinglot_id', '')
+
+            if parkinglot_id:
+                ctx['parkinglot_id'] = int(parkinglot_id)
+                groundSensors = groundsensors.filter(parkinglot_id=int(parkinglot_id))
+                ctx['gates'] = Gate.objects.filter(parkinglot_id=int(parkinglot_id))
+            if gate_id:
+                ctx['gate_id'] = int(gate_id)
+                groundsensors = groundsensors.filter(gate_id=int(gate_id))
+            if brand:
+                groundsensors = groundsensors.filter(brand=brand)
+            if manufacturer:
+                groundsensors = groundsensors.filter(manufacturer=manufacturer)
+                
+        elif action == 'delete':
+            ids = request.POST.getlist('ids', '')
+            GroundSensor.objects.filter(id__in=ids).delete()
+
+        elif action == 'validate':
+            number = request.POST.get('number', '')
+            id = request.POST.get('id', '')
+
+            r = GroundSensor.objects.filter(number=number.strip())
+            if r.exists():
+                if id:
+                    if r.first().id != int(id):
+                        return JsonResponse({'valid': False})
+                else:
+                    return JsonResponse({'valid': False})
+
+            return JsonResponse({'valid': True})
+
+        elif action == 'get_gate':
+            id = request.POST.get('id', '')
+            gates = Gate.objects.filter(parkinglot_id=id)
+            gates = [{'name': _.name, 'id':_.id} for _ in gates]
+            return JsonResponse({'success':True, 'result': gates})
+
+    ctx['objects'] = groundsensors.order_by('-buy_time')
+    ctx['parkinglots'] = ParkingLot.objects.filter(status=0).all()
+    print(ctx['parkinglots'])
+    all_gates = {}
+
+    gates = Gate.objects.select_related('parkinglot').filter(parkinglot__status=0).order_by('parkinglot')
+    for i in gates:
+        if i.parkinglot.id in all_gates:
+            all_gates[i.parkinglot.id].append({'gate_id': i.id, 'gate_name': i.monitor})
+        else:
+            all_gates[i.parkinglot.id] = [{'gate_id': i.id, 'gate_name': i.monitor}]
+    ctx['all_gates'] = all_gates
+
+    return (ctx, 'groundsensor.html')
