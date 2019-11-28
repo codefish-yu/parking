ac7861ca7558782c7a1e8c4ae306ff939435f1c4
CherryXie <cherryxie@xiaodeMacBook-Pro.local>
2019-11-28 12:04:03 +0800

    xc

--- parking/company/models.py
+++ parking/company/models.py
@@ -9,9 +9,10 @@ class Company(models.Model):
 	class Meta:
 		verbose_name = verbose_name_plural = '商户管理'
 
-	status = models.IntegerField(default=0,verbose_name='状态',choices=[(0,'冻结'),(1,'激活')])
-	name = models.CharField(max_length=30, verbose_name='商家名称')
-	account = models.CharField(max_length=30,unique=True,verbose_name='登录账号')
+	status = models.IntegerField(default=0,verbose_name='状态',choices=[(-1,'删除'),(0,'冻结'),(1,'激活')])
+	name = models.CharField(max_length=30, null=True,verbose_name='商家名称')
+	address = models.CharField(max_length=30,null=True,verbose_name='商家地址')
+	account = models.CharField(max_length=30,verbose_name='登录账号')
 	owner = models.CharField(max_length=30,verbose_name='联系人姓名')
 	phone = models.IntegerField(default=0,verbose_name='联系人电话')
 	balance = models.FloatField(default=0,verbose_name='余额')
@@ -20,6 +21,20 @@ class Company(models.Model):
 	rule = models.CharField(max_length=100,null=True,verbose_name='限制规则')
 	amount = models.IntegerField(default=0,verbose_name='限制张数')
 	descript = models.CharField(max_length=100,verbose_name='商家描述',null=True)
+
+
+class Refund(models.Model):
+	class Meta:
+		verbose_name = verbose_name_plural = '退款记录'
+
+
+	company = models.ForeignKey('Company',null=True,on_delete=models.CASCADE)
+	money = models.FloatField(default=0,verbose_name='退款金额')
+	duration = models.FloatField(default=0,verbose_name='退款时长')
+	cost = models.FloatField(default=0,verbose_name='退还费用')
+	reason = models.CharField(max_length=100,verbose_name='退款原因')
+			
+		
 
 
 
