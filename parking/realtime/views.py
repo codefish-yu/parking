b845ec142db3125af2253433cc8a948b155e71f4
nyx <nyx@metatype.cn>
2019-12-19 19:25:29 +0800

    xc

--- parking/realtime/views.py
+++ parking/realtime/views.py
@@ -54,7 +54,7 @@ def parkin(request):
     # }
 
     params = get_params(request)
-    print(params['cam_id'])
+    print(params)
     
     cam_id = params['cam_id']
     camera = Camera.objects.filter(mac_address=cam_id).first()
