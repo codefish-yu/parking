48d8106f86432ab4ad09a73348d3e20b4d41d2a9
codefish-yu <yutaixin@foxmail.com>
2020-01-07 22:28:29 +0800

    first day

--- parking/manager/views.py
+++ parking/manager/views.py
@@ -10,18 +10,18 @@ def manage_login(request):	#username:管理员名字
 	ctx = {}
 
 	if request.method == 'POST':
-		username = request.POST.get('user_name')	#user_name为html页面元素中name
+		username = request.POST.get('account')	#user_name为html页面元素中name
 		if not username:
 			dic = {'msg':'请提交用户名'}
 			return render(request,'manage_login.html',dic)
 
-		password = request.POST.get('user_pass')
+		password = request.POST.get('password')
 		if not password:
 			dic = {'msg':'请提交密码'}
 			return render(request,'manage_login.html',dic)
 
 		#查找用户
-		user = AdminUser.object.filter(user_name=username)	
+		user = AdminUser.objects.filter(user_name=username)
 
 		#用户不存在
 		if not user:
@@ -29,14 +29,14 @@ def manage_login(request):	#username:管理员名字
 			return render(request,'manage_login.html',dic)
 
 		#密码不正确
-		if user[0].password != password:
+		if user[0].user_pass != password:
 			dic = {'msg':'用户名或密码错误'}
 			return render(request,'manage_login.html',dic)
 
 		#用户名和密码正确，存session进入首页
 		request.session['username'] = username
 
-		return HttpResponseRedirect('首页')
+		return render(request,'manage_home.html')
 
 
 	return render(request,'manage_login.html',ctx)
